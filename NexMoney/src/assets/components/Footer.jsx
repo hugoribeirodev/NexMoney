59eb6e7959b6ea7dedebc8454a1e15b2a2fbcb7f
Hugo-Ribeiro1 <hugorsousa44@gmail.com>
2024-10-26 23:31:18 -0300

    Fintech finalizada

--- NexMoney/src/assets/components/Footer.jsx
+++ NexMoney/src/assets/components/Footer.jsx
@@ -1,9 +1,34 @@
 import styles from '../styles/Footer.module.css'
+import Logo from '../img/Logo.svg'
 
 function Footer(){
     return(
-        <footer>
-            <h1>ola</h1>
+        <footer className={styles.footer}>
+            <div className={styles.divpaifooter}>
+                <div className={styles.divpailist}>
+                    <div className={styles.divnexmoney}>
+                        <h1>NexMoney</h1>
+                        <li>Sobre nós</li>
+                        <li>Perguntas Frequentes</li>
+                        <li>Contato</li>
+                    </div>
+                    <div className={styles.divconosco}>
+                        <h1>Fale Conosco</h1>
+                        <li>4002 8922</li>
+                        <li>meajuda@nexmoney.com.br</li>
+                    </div>
+                    <div className={styles.divprodutos}>
+                        <h1>Produtos</h1>
+                        <li>Conta Digital</li>
+                        <li>Cartão de Crédito</li>
+                        <li>NexMoney Darkblue</li>
+                    </div>
+                </div>
+                    <div className={styles.divdireitos}>
+                        <img src={Logo} alt="Logo" />
+                        <p>© 2024 NexMoney. <br />Todos os direitos reservados.</p>
+                    </div>
+            </div>
         </footer>
     )
 }
--- NexMoney/src/assets/pages/Home.jsx
+++ NexMoney/src/assets/pages/Home.jsx
@@ -1,8 +1,81 @@
 import styles from '../styles/Home.module.css'
+import card from '../img/cartaobola.svg'
+import emprestimo from '../img/imgemprestimo.svg'
+import seguro from '../img/imagemseguro.svg'
 
 function Home(){
     return(
-        <h1>Ola</h1>
+        <section>
+            <section className={styles.section1}>
+                <div className={styles.divimage}>
+                    <div className={styles.imageazul}>
+                        <h1>Invista com a força de um touro. construa seu legado financeiro.</h1>
+                        <h5>Na NexMoney você cresce suas finanças com força e confiança.</h5>
+                    </div>
+                </div>
+            </section>
+
+            <section className={styles.sectionservicos}>
+                <div className={styles.holan}>
+                    <div className={styles.samescard}>
+                        <h2>Cartão de Crédito</h2>
+                        <p>Descubra um cartão moderno e gratuito, sem anuidade. Mantenha o controle total das suas finanças com a NexMoney e faça seu dinheiro trabalhar para você.</p>
+                        <p>Veja mais</p>
+                    </div>
+
+                    <div className={styles.samescard}>
+                        <h2>Investimentos</h2>
+                        <p>Comece sua jornada de investimentos com apenas R$ 1! Na NexMoney, oferecemos opções personalizadas para cada perfil. Desfrute de uma experiência intuitiva, segura e transparente, onde seu dinheiro pode crescer sem complicações.</p>
+                        <p>Veja mais</p>
+                    </div>
+
+                    <div className={styles.samescard}>
+                        <h2>Carteira Digital</h2>
+                        <p>Guarde os dados do seu cartão de crédito com segurança nas carteiras digitais do Google Pay, Apple Pay e Samsung Pay. Com a NexMoney, sua praticidade e segurança estão sempre em primeiro lugar!</p>
+                        <p>Veja mais</p>
+                    </div>
+                </div>
+            </section>
+
+            <section className={styles.sectionamarela}>
+                <div className={styles.divtexto}>
+                    <h1>Realize pagamentos seguros para qualquer lugar do mundo! Com a flexibilidade de escolher seu meio de pagamento preferido, a NexMoney transforma seu smartphone em uma ferramenta poderosa para suas finanças. <span>Quem diria que um aplicativo poderia fazer tanto?</span></h1>
+                </div>
+            </section>
+
+            <section className={styles.sectioncard}>
+                <div className={styles.divgray}>
+                    <div className={styles.divcontent}>
+                        <h2>Conheça o Cartão de Crédito NexMoney</h2>
+                        <p>Conheça o 'Azulzinho': o cartão moderno, gratuito e prático da NexMoney! Realize pagamentos seguros para qualquer lugar do mundo com a flexibilidade de escolher seu meio de pagamento preferido.</p>
+                        <p className={styles.veja}>Veja mais</p>
+                    </div>
+
+                    <div className={styles.cartao}>
+                        <img src={card} alt="Cartão NexMoney" />
+                    </div>
+                </div>
+            </section>
+
+            <section className={styles.sectionmais}>
+                <h1 className={styles.mstitle}>Mais Serviços NexMoney</h1>
+                <div className={styles.twocards}>
+                    <div className={styles.samescards2}>
+                        <img src={emprestimo} alt="Emprestimo" />
+                        <h2>Emprestimo</h2>
+                        <p>Empréstimos feitos para você: com total transparência e segurança, da simulação até a última parcela. Na NexMoney, você tem o controle nas suas mãos, garantindo que cada passo seja do seu jeito!</p>
+                        <p className={styles.vejaMais}>Veja Mais</p>
+                    </div>
+                    <div className={styles.samescards2}>
+                        <img src={seguro} alt="Seguro Celular" />
+                        <h2>Seguro Celular</h2>
+                        <p>Proteja seu celular e aproveite ao máximo tudo o que ele pode oferecer! Com o seguro da NexMoney, você fica tranquilo para curtir cada momento, sem se preocupar com imprevistos.</p>
+                        <p className={styles.vejaMais}>Veja Mais</p>
+                    </div>
+                </div>
+            </section>
+        </section>
+        
     )
 }
 
